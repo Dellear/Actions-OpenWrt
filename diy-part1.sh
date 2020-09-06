33d97f3635c67957c1a2ab582dbb7c6553853b8a
Dellear <358009775@qq.com>
2020-09-06 23:08:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,4 @@
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/V2RaySSR/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/V2RaySSR/small' feeds.conf.default
+sed -i '$a src-git rosy https://github.com/rosywrt/luci.git' feeds.conf.default
